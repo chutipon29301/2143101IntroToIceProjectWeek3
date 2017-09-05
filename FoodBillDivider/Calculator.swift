c99d5a7d33325548cd3c0f88993ff00c67f83a90
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-09-05 21:19:20 +0700

    Add gitignore and add swift files

---
+++ FoodBillDivider/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  FoodBillDivider
+//
+//  Created by admin on 9/5/2560 BE.
+//  Copyright © 2560 MonkeyIT. All rights reserved.
+//
+
+import Foundation
---
+++ FoodBillDivider/History.swift
@@ -0,0 +1,9 @@
+//
+//  History.swift
+//  FoodBillDivider
+//
+//  Created by admin on 9/5/2560 BE.
+//  Copyright © 2560 MonkeyIT. All rights reserved.
+//
+
+import Foundation
